3f13111daf50974201e51978c73be4b4ae9a1a35
petir26 <jagowanmuda@gmail.com>
2024-09-21 13:52:18 +0800

    Add files via upload

---
+++ LuasPersegi.java
@@ -0,0 +1,10 @@
+package tipedata;
+
+
+public class LuasPersegi {
+    public static void main(String[] args) {
+        int sisi = 3;
+        int luas = sisi*sisi;
+        System.out.println("Luas Persegi : " + luas);
+    }
+}
